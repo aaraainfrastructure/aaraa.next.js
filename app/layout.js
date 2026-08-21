9eaabb6910d6b7f427edcccf7a789a1fe5897ffe
AARAA Infrastructure <aaraainfrastructure@gmail.com>
2026-08-21 11:44:17 +0530

    fix(analytics): remove unused gtm.js snippet to standardize on gtag/js

--- app/layout.js
+++ app/layout.js
@@ -35,16 +35,6 @@ export default function RootLayout({children}){
           `}
         </Script>
 
-        {/* Google Tag Manager (GTM) */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PLACEHOLDER');
-          `}
-        </Script>
 
         {/* Microsoft Clarity */}
         <Script id="microsoft-clarity" strategy="afterInteractive">
